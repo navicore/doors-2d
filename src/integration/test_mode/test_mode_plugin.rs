3fd77cdffb3d3d58a958312672eddd10327d4b19
Ed Sweeney <ed@onextent.com>
2025-01-26 19:33:18 -0800

    refactor

--- src/integration/test_mode/test_mode_plugin.rs
+++ src/integration/test_mode/test_mode_plugin.rs
@@ -13,26 +13,19 @@ impl Plugin for TestModeIntegrationPlugin {
 mod tests {
     use super::*;
     use crate::floorplan::FloorPlanEvent;
-    use bevy::ecs::event::Events;
 
     #[test]
     fn test_fire_floor_plan_event() {
-        // Create a new Bevy app
+        // Create a new Bevy app and add the necessary plugins and events
         let mut app = App::new();
-
-        // Add the DummyIntegrationPlugin
         app.add_plugins(TestModeIntegrationPlugin);
-
-        // Add the FloorPlanEvent
         app.add_event::<FloorPlanEvent>();
 
         // Run the startup systems
         app.update();
 
-        // Get the world from the app
+        // Get the world from the app and retrieve the FloorPlanEvent resource
         let world = app.world();
-
-        // Check if the FloorPlanEvent was fired
         let events = world.get_resource::<Events<FloorPlanEvent>>().unwrap();
         let mut event_reader = events.get_cursor();
         let events: Vec<&FloorPlanEvent> = event_reader.read(events).collect();
@@ -40,14 +33,14 @@ mod tests {
         // Assert that one event was fired
         assert_eq!(events.len(), 1);
 
-        let plan = events[0];
-        let start_room = plan.floor_plan.get_start_room().unwrap();
+        // Validate the floor plan event details
+        let plan = &events[0].floor_plan;
+        let start_room = plan.get_start_room().unwrap();
         assert_eq!(start_room.id, "1");
-        let doors_and_rooms = plan
-            .floor_plan
-            .get_doors_and_connected_rooms(&start_room.id)
-            .unwrap();
+
+        let doors_and_rooms = plan.get_doors_and_connected_rooms(&start_room.id).unwrap();
         assert_eq!(doors_and_rooms.len(), 1);
+
         let other_room = doors_and_rooms[0].1;
         assert_eq!(other_room.id, "2");
     }
