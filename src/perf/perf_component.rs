66825048fb32ac7d88b5af8907af41d70b040259
Ed Sweeney <ed@onextent.com>
2025-03-08 12:15:14 -0800

    2024

--- src/perf/perf_component.rs
+++ src/perf/perf_component.rs
@@ -5,7 +5,6 @@ use bevy::ecs::system::SystemParam;
 use bevy::prelude::*;
 use iyes_perf_ui::entry::PerfUiEntry;
 use iyes_perf_ui::prelude::*;
-use iyes_perf_ui::ui::root::PerfUiPosition;
 
 #[derive(Component)]
 pub struct GameWorldMonitor;
